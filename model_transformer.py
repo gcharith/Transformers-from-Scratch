c36338739c5e86a7ad8ceab2c7185b6431704edd
Sri <charithgrandhi@gmail.com>
2025-03-12 04:30:55 -0400

    multi head attention to decoder

--- model_transformer.py
+++ model_transformer.py
@@ -61,5 +61,106 @@ class feedForward(nn.Module):
     def forward(self,x):
         return self.linear_2(self.dropout(torch.relu(self.linear_1(x))))
 
+class multiHeadAttention(nn.Module):
+    def __init__(self, d_model: int, heads: int, dropout: float) -> None:
+        super().__init__()
+        self.d_model = d_model
+        self.h = heads
+        assert d_model % heads == 0, "D_model is not divisible by number of heads"
 
+        self.d_k = self.d_model//self.h
+
+        #defining Wq, Wk, Wv, Wo
+        self.w_q = nn.Linear(d_model,d_model)  
+        self.w_k = nn.Linear(d_model,d_model)
+        self.w_v = nn.Linear(d_model,d_model)
+        self.w_o = nn.Linear(d_model,d_model)
+
+    @staticmethod
+    def attention(query, key, value, mask, dropout: nn.Dropout):
+        d_k = query.shape[-1]
+
+        attention_score = ((query @ key.transpose(-2,-1))/math.sqrt(d_k))
+
+        if mask is not None:
+            attention_score.masked_fill_(mask==0,-1e9)
+        attention_score = torch.softmax(attention_score, dim=-1)
+    
+        return (attention_score @ value), attention_score # returning tuple for visualization
         
+
+    def forward(self, q, k, v, mask):
+        self.q = self.w_q(q)
+        self.k = self.w_k(k)
+        self.v = self.w_v(v) 
+
+        query = query.view(query.shape[0],query.shape[1],self.h, self.d_k).transpose(1,2)
+        key = key.view(key.shape[0],key.shape[1],self.h, self.d_k).transpose(1,2)
+        value = value.view(value.shape[0],value.shape[1],self.h, self.d_k).transpose(1,2)
+        
+        x,self.attention_score = multiHeadAttention.attention(query,key,value,mask,self.dropout)
+
+        x = x.transpose(1,2).contiguous().view(x.shape[0],x.shape[1], -1, self.h*self.d_k) #changing back to original dimensions
+
+        return self.w_o(x)
+
+class skipConnection(nn.Module):
+    def __init__(self, dropout: float):
+        super().__init__()
+        self.dropout = nn.Dropout(dropout)
+        self.norm = layerNormalization()
+    
+    def forward(self, x, sublayer):
+        return x + self.dropout(sublayer(self.norm(x)))  # is norm before sublayer?
+    
+class encoderBlock(nn.Module):
+    def __init__(self, attention_block: multiHeadAttention, feed_forward: feedForward, dropout: float) -> None:
+        self.attention_block = attention_block
+        self.feed_forward = feed_forward
+        self.skip_connections = nn.ModuleList([skipConnection(dropout) for i in range(2)])
+
+    def forward(self, x, src_mask): #src_mask masks the interaction of the padding words with our tokens in input of the encoder
+        x = self.skip_connections[0](x,lambda x: self.attention_block(x,x,x,src_mask))
+
+        x = self.skip_connections[1](x,self.feed_forward)
+
+        return x
+    
+class encoder(nn.Module):
+    def __init__(self, layers: nn.ModuleList)->None:
+        super().__init__()
+        self.layers = layers
+        self.norm = layerNormalization()
+    
+    def forward(self, x, mask):
+        for layer in self.layers:
+            x = layer(x,mask)
+        
+        return self.norm(x)
+    
+class decoderBlock(nn.Module):
+    def __init__(self,masked_attention_block: multiHeadAttention, cross_attention_block: multiHeadAttention, feed_forward: feedForward, dropout: float) -> None:
+        self.masked_attention_block = masked_attention_block
+        self.cross_attention_block = cross_attention_block
+        self.feed_forward =  feed_forward
+        self.skip_connections = nn.ModuleList(skipConnection(dropout) for i in range(3))
+    
+    def forward(self, x, encoder_output, src_mask, target_mask):
+        x = self.skip_connections[0](x, lambda x: self.masked_attention_block(x,x,x,target_mask))
+
+        x = self.skip_connections[1](x, lambda x: self.cross_attention_block(x,encoder_output,encoder_output,src_mask))
+
+        x = self.skip_connections[2](x, self.feed_forward)
+
+        return x
+    
+class decoder(nn.Module):
+    def __init__(self, layers: nn.ModuleList):
+        super().__init__()
+        self.layers = layers
+        self.norm = layerNormalization()
+    
+    def forward(self, x, encoder_output, src_mask, target_mask):
+        for layer in self.layers:
+            x = layer(x, encoder_output, src_mask, target_mask)
+        return self.norm(x)
